0eb0fbd9886a8811d67f776a80fe5741fd973bb5
felipehenrique159 <felipecod6@hotmail.com>
2020-02-22 14:57:18 -0300

    instalação do cors

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')  //imports
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json()) // to allow json in project
-
+app.use(cors()) // allow access for all domains
 
 app.listen(3000) //port connection
 
