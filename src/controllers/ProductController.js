e410ca2b0445f57ad06e30a853884c0346c023c2
felipehenrique159 <felipecod6@hotmail.com>
2020-02-21 23:19:44 -0300

    req.body não retorna nada

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,16 +2,39 @@ const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
 
 module.exports = {
+
     async index(req, res) {
-        const products = await Product.find();
-    
+        const products = await Product.find();  //return all products
+
         return res.json(products)
 
     },
 
     async store(req, res) {
-        const product = await Product.create(req.body);
-        
+
+        try {
+            const product = await Product.create(req.body) //body requisition
+
+            return res.json(product) //return json
+
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    async show(req, res) {
+        const product = await Product.findById(req.params.id) //search for products id
         return res.json(product)
-    }
+    },
+
+    async update(req, res) {
+        try {
+
+            const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true })  //new = true , for get new product
+            return product
+
+        } catch (error) {
+            console.log(error)
+        }
+    },
 }
